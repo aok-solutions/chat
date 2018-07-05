497a829fe8599d7d774797d60f66e5f5fa6e91aa
Ayanga Okpokowuruk <ayanga@aok-solutions.com>
2018-07-05 09:01:13 -0400

    integrate socket.io

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 let app = require('express')()
 let http = require('http').Server(app)
+let io = require('socket.io')(http)
 let port = 3000
 
 app.get('/', (req,res) => {
   res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
 })
 
 http.listen(port, () => {
